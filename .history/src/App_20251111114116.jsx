b525dea0dff7b7e1e1d3654cf427d96d378a7a70
Moustapha Bah <bahmouhamadoumoustapha@gmail.com>
2025-11-11 11:59:34 +0000

    Initial commit

---
+++ .history/src/App_20251111114116.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-white'>
+      BONJOUR ET BIENVENUE
+    </div>
+  )
+}
+
+export default App
---
+++ .history/src/App_20251111114259.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-white bg-red-500 p-4'>
+      BONJOUR ET BIENVENUE
+    </div>
+  )
+}
+
+export default App
